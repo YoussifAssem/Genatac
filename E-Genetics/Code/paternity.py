bc08eb240d02a398e41189be715c2fbeb0e0be1c
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2022-02-18 00:23:10 +0200

    Update Code and Done will code
    1) Add Package cryptocode will use it in GUI
    2) Add Image family-law
    3) Update code paternity to add results in separate screen
    4) Add Data to make code on chromosome
    5) Remove image street will not be needed
    6) Add Rule to calculate percentage

--- E-Genetics/Code/paternity.py
+++ E-Genetics/Code/paternity.py
@@ -1,18 +1,22 @@
 
+from multiprocessing.sharedctypes import Value
 from os import chmod
 from re import L
 from turtle import color
 import pandas as pd
 from paternityClass import paternityTest
-from tkinter import Canvas, Tk, Label, Button, Entry, END 
+from tkinter import Canvas, Tk, Label, Button, Text, END, Toplevel 
 from PIL import ImageTk
 from PIL import Image  
 from tkinter.filedialog import askopenfile 
+
+     
 def open_file(): 
     file = askopenfile(mode ='r', filetypes =[('Python Files', '*.csv')]) 
     if file is not None: 
         data = pd.read_csv(file.name)
         obj = paternityTest()
+        
         rsSimilar = obj.check(data['father'], data['mother'], data['child1'], data['combine'])[0]
         rsFather =  obj.check(data['father'], data['mother'], data['child1'], data['combine'])[4]
         rsMother =  obj.check(data['father'], data['mother'], data['child1'], data['combine'])[5]
@@ -29,82 +33,76 @@ def open_file():
         mother = obj.check(data['father'], data['mother'], data['child1'], data['combine'])[7]
         chFather = obj.check(data['father'], data['mother'], data['child1'], data['combine'])[8]
         chMother = obj.check(data['father'], data['mother'], data['child1'], data['combine'])[9]
+    
+        sumSimilar = len(rsSimilar)
+        total = (len(rsSimilar)+ len(rsFather))
+        sumNotSimilar = len(rsFather)
+        rule = sumSimilar / total
+        ruleNotSimilar = sumNotSimilar / total
+         
+        viewScreen = Toplevel(root)
+        viewScreen.title("Results")
+        viewScreen.geometry("645x400")
 
-        Label(root, text= ('The Number of rsNumbers')).place(x= 400, y=0)
-        Label(root, text= (len(rsSimilar)+ len(rsFather)+len(rsMother)-4), fg='Red').place(x= 450, y=25)   
+        T = Text(viewScreen, height=25, width=80, bg='black', fg='white')
+        T.place(x= 0, y=0)
+        T.insert(END, "The Number of rsNumbers: {}".format(len(rsSimilar)+ len(rsFather)+len(rsMother)))
+        T.insert(END, "\n----------------------------------------")
+        T.insert(END, "\nThe Number of rsNumbers fit the rule: {}".format(len(rsSimilar)))
+        T.insert(END, "\nRs Numbers: {}".format(rsSimilar[0:5]))
+        T.insert(END, "\nFather: {}".format(fatherSimilar[0:5]))
+        T.insert(END, "\nChild: {}".format(childSimilar[0:5]))
+        T.insert(END, "\nMother: {}".format(motherSimilar[0:5]))
+        
+        T.insert(END, "\n----------------------------------------")
+        T.insert(END, "\nThe Number of rsNumbers does not fit the rule child with father: {}".format(len(rsFather)))
+        T.insert(END, "\nDf rsNumber: {}".format(rsFather[0:5]))
+        T.insert(END, "\nDf Father: {}".format(father[0:5]))
+        T.insert(END, "\nDf Child: {}".format(chFather[0:5]))
+       
+        T.insert(END, "\n----------------------------------------")
+        T.insert(END, "\nThe Number of rsNumbers does not fit the rule child with mother: {}".format(len(rsMother)))
+        T.insert(END, "\nDf rsNumber: {}".format(rsMother[0:5]))
+        T.insert(END, "\nDf Mother: {}".format(mother[0:5]))
+        T.insert(END, "\nDf Child: {}".format(chMother[0:5]))
+        T.insert(END, "\n----------------------------------------")
+        T.insert(END, "\nSo, The Probability this may be the Fathe: {}".format(rule * 100))
+        T.insert(END, "\nSo, The Probability this may Not be the Father: {}".format(ruleNotSimilar * 100))
+       
+        T.config(state='disabled')
+        
         print('The Number of rsNumbers', len(rsSimilar)+ len(rsFather)+len(rsMother)-4)
-       
-        Label(root, text= ('The Number of rsNumbers fit the rule')).place(x= 400, y=50)
-        Label(root, text= (len(rsSimilar)), fg='Red').place(x= 450, y=75)
         print('\n\nThe Number of rsNumbers fit the rule', len(rsSimilar))
-       
-        Label(root, text= ('The Similar rsNumber')).place(x= 400, y=100)
-        Label(root, text= (rsSimilar[0:5]), fg='Red').place(x= 450, y=125)
         print('The Similar rsNumber', rsSimilar[0:5])  
-       
-        Label(root, text= ('Father')).place(x= 400, y=150)
-        Label(root, text= (fatherSimilar[0:5]), fg='Red').place(x= 450, y=175)
-        print('Father', fatherSimilar[0:5])
-       
-        Label(root, text= ('Child')).place(x= 400, y=200)
-        Label(root, text= (childSimilar[0:5]), fg='Red').place(x= 450, y=225)
+        print('Father: ', fatherSimilar[0:5])
         print('Child', childSimilar[0:5])
-       
-        Label(root, text= ('Mother')).place(x= 400, y=250)
-        Label(root, text= (motherSimilar[0:5]), fg='Red').place(x= 450, y=275)
         print('Mother', motherSimilar[0:5])
-      
-        Label(root, text= 'Difference between Father and Child').place(x= 400, y=300)
         print('\n\nDifference between Father and Child')
-       
-        Label(root, text= ('The Number of rsNumbers does not fit the rule child with father')).place(x= 400, y=325)
-        Label(root, text= (len(rsFather)), fg='Red').place(x= 450, y=350)
         print('The Number of rsNumbers does not fit the rule child with father', len(rsFather))
-
-        Label(root, text= ('Df rsNumber')).place(x= 400, y=375)
-        Label(root, text= (rsFather[0:5]), fg='Red').place(x= 450, y=400)
         print('Df rsNumber', rsFather[0:5])
-       
-        Label(root, text= ('Df Father')).place(x= 400, y=425)
-        Label(root, text= (father[0:5]), fg='Red').place(x= 450, y=450)
         print('Df Father', father[0:5])
-       
-        Label(root, text= ('Df Child')).place(x= 400, y=475)
-        Label(root, text= (chFather[0:5]), fg='Red').place(x= 450, y=500)
         print('Df Child', chFather[0:5])
-       
-        Label(root, text= 'Difference between Mother and Child').place(x= 400, y=525)
         print('\n\nDifference between Mother and Child')
-
-        Label(root, text= ('The Number of rsNumbers does not fit the rule child with mother')).place(x= 400, y=550)
-        Label(root, text= (len(rsMother)), fg='Red').place(x= 450, y=575)
         print('The Number of rsNumbers does not fit the rule child with mother', len(rsMother))
-       
-        Label(root, text= ('Df rsNumber')).place(x= 400, y=600)
-        Label(root, text= (rsMother[0:5]), fg='Red').place(x= 450, y=625)
         print('Df rsNumber', rsMother[0:5])
-       
-        Label(root, text= ('Df Child')).place(x= 400, y=650)
-        Label(root, text= (chMother[0:5]), fg='Red').place(x= 450, y=675)
         print('Df Child', chMother[0:5])
-       
-        Label(root, text= ('Df Mother')).place(x= 400, y=700)
-        Label(root, text= (mother[0:5]), fg='Red').place(x= 450, y=725)
         print('Df Mother', mother[0:5])
-
+        print("So, The Probability this may be the Father: ", rule * 100)
+        print("So, The Probability this may Not be the Father: ", ruleNotSimilar * 100)
+      
 if __name__ == "__main__":
   root = Tk()  
   root.title('paternityTest')
-  canvas = Canvas(root, width = 900, height = 800)  
+  canvas = Canvas(root, width = 500, height = 500)  
   canvas.pack()
-  load = Image.open("street.png")
+  load = Image.open("family-law.png")
   render = ImageTk.PhotoImage(load) 
   img = Label(image=render)
   img.image = render
   img.place(x=0, y=0)
-  btn = Button(root, text= 'Browse',  command= lambda:open_file())
+  btn = Button(root, text= 'Browse',  command= lambda:open_file(), background='black',fg='white')
   
-  btn.place(x= 80, y=280)
+  btn.place(x= 200, y=450)
   root.mainloop()  
   
 
--- E-Genetics/Code/paternityClass.py
+++ E-Genetics/Code/paternityClass.py
@@ -20,7 +20,12 @@ class paternityTest:
   mo = []
   chFather = []
   chMother = []
-   
+  chromosome = []
+  probFather = 0
+  probMother = 0
+  probChild = 0
+  
+
   def check(self, father, mother, child, rs):
     for f, m, c, r in zip(father, mother, child, rs):
         '''2 2 2'''
@@ -171,4 +176,184 @@ class paternityTest:
             self.chMother.append(c)
     return self.rsNumberSimilar, self.fatherSimilar, self.motherSimilar, self.childSimilar, self.rsNumbersFather, self.rsNumbersMother, self.fa, self.mo, self.chFather, self.chMother                   
       
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+def checkChromosome(self, father, mother, child, rs, chromosome):
+    for f, m, c, r, chro, i in zip(father, mother, child, rs, chromosome, range(23)):
+        '''2 2 2'''
+        if(chro == i+1 or chro == 'X' or chro == 'Y' or chro == 'MT'):  
+         if(len(c) == 2 and len(f) == 2 and len(m) == 2): 
+          if(f[0] == c[0] or f[0] == c[1] or f[1] == c[0] or f[1] == c[1]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          if(m[0] == c[0] or m[0] == c[1] or m[1] == c[0] or m[1] == c[1]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0] and f[0] != c[1] and f[1] != c[0] and f[1] != c[1]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c)
+            self.chromosome.append(chro)
+          if(m[0] != c[0] and m[0] != c[1] and m[1] != c[0] and m[1] != c[1]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c)
+            self.chromosome.append(chro)
+          
+
+            '''2 2 1'''
+         if(len(c) == 2 and len(f) == 2 and len(m) == 1): 
+          if(f[0] == c[0] or f[0] == c[1] or f[1] == c[0] or f[1] == c[1]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          if(m[0] == c[0] or m[0] == c[1]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0] and f[0] != c[1] and f[1] != c[0] and f[1] != c[1]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c)  
+            self.chromosome.append(chro)
+          if(m[0] != c[0] and m[0] != c[1]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c)  
+            self.chromosome.append(chro)
+          
+            '''2 1 2'''
+         if(len(c) == 2 and len(f) == 1 and len(m) == 2): 
+          if(f[0] == c[0] or f[0] == c[1]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          if(m[0] == c[0] or m[0] == c[1] or m[1] == c[0] or m[1] == c[1]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0] and f[0] != c[1]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c)
+            self.chromosome.append(chro)
+          if(m[0] != c[0] and m[0] != c[1] and m[1] != c[0] and m[1] != c[1]):
+            self.rsNumbersMother.append(m)
+            self.mo.append(m)
+            self.chMother.append(c)
+            self.chromosome.append(chro)
+                 
+          '''2 1 1'''
+         if(len(c) == 2 and len(f) == 1 and len(m) == 1): 
+          if(f[0] == c[0] or f[0] == c[1]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          
+          if(m[0] == c[0] or m[0] == c[1]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0] and f[0] != c[1]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c)
+            self.chromosome.append(chro)
+          
+          if(m[0] != c[0] and m[0] != c[1]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c)        
+            self.chromosome.append(chro)
+          
+         '''1 2 2'''
+         if(len(c) == 1 and len(f) == 2 and len(m) == 2):
+          if(f[0] == c[0] or f[1] == c[0]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          
+          if(m[0] == c[0] or m[1] == c[0]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0] and f[1] != c[0]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c) 
+            self.chromosome.append(chro)
+          
+          if(m[0] != c[0] and m[1] != c[0]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c)    
+            self.chromosome.append(chro)
+          
+         '''1 2 1'''
+         if(len(c) == 1 and len(f) == 2 and len(m) == 1):
+          if(f[0] == c[0] or f[1] == c[0]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          if(m[0] == c[0]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0] and f[1] != c[0]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c) 
+            self.chromosome.append(chro)
+          if(m[0] != c[0]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c) 
+            self.chromosome.append(chro)
+              
+          '''1 1 2'''
+         if(len(c) == 1 and len(f) == 1 and len(m) == 2):
+          if(f[0] == c[0]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          if(m[0] == c[0] or m[1] == c[0]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c)
+            self.chromosome.append(chro)
+          if(m[0] != c[0] and m[1] != c[0]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c)
+            self.chromosome.append(chro)
+                 
       
+         '''1 1 1'''
+         if(len(c) == 1 and len(f) == 1 and len(m) == 1): 
+          if(f[0] == c[0]):
+             self.rsNumberSimilar.append(r)
+             self.fatherSimilar.append(f)
+             self.childSimilar.append(c)
+             self.chromosome.append(chro)
+          if(m[0] == c[0]):
+             self.motherSimilar.append(m)
+          
+          if(f[0] != c[0]):
+            self.rsNumbersFather.append(r)
+            self.fa.append(f)
+            self.chFather.append(c)
+            self.chromosome.append(chro)
+          if(m[0] != c[0]):
+            self.rsNumbersMother.append(r)
+            self.mo.append(m)
+            self.chMother.append(c)
+            self.chromosome.append(chro)
+          
+    return self.rsNumberSimilar, self.fatherSimilar, self.motherSimilar, self.childSimilar, self.rsNumbersFather, self.rsNumbersMother, self.fa, self.mo, self.chFather, self.chMother                   
+       
