0bfa17363417ee6924819d9334165e138be5ef77
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2022-04-30 18:00:16 +0200

    Update to run mobile
    Update code and power point names

--- E-Genetics/Code/paternity_app/lib/main.dart
+++ E-Genetics/Code/paternity_app/lib/main.dart
@@ -2,19 +2,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+//TO RUN WEB REMOVE THESES
+/*
 import 'Screens/web/web_screen/splash_screen.dart';
-/*import 'Screens/splash_screen.dart';*/
 import 'Screens/web/web_screen/login_screen.dart';
-/*import 'Screens/login_screen.dart';*/
+*/
+//MOBILE SCREENS
+import 'Screens/splash_screen.dart';
+import 'Screens/login_screen.dart';
 
 //import 'Screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //To Run Mobile App remove this comment and run
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
 //To Run Web App PLEASE if you will run the web RUN WITHOUT DEBUGGING remove this comment and run
 
+/* 
   await Firebase.initializeApp(
       options: const FirebaseOptions(
     apiKey: "AIzaSyCmuZGl5AWSah3INjLhA7fpZhXXMMtT1wk",
@@ -22,6 +27,7 @@ Future<void> main() async {
     messagingSenderId: "86992490246",
     projectId: "paternitytest-7cb8b",
   ));
+  */
   runApp(MyApp());
 }
 
