4ff9f5d7034a5c8c5746566fdf072617e2b53f1f
kareem644 <kareem1802405@miuegypt.edu.eg>
2022-05-25 00:40:37 +0200

    homescreen ui

--- E-Genetics/Code/paternity_app/lib/Screens/home_screen.dart
+++ E-Genetics/Code/paternity_app/lib/Screens/home_screen.dart
@@ -15,35 +15,62 @@ class _homeScreen extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 38, 54, 80),
+      appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 38, 54, 80),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 38, 54, 80),
-          title: const Text(
-            'Home Page',
-          ),
-          actions: [
-            Container(
-              alignment: Alignment.centerRight,
-              child: Text(
-                user.Email,
-                style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic),
-              ),
-            )
-          ],
+        title: const Text(
+          'Welcome',
         ),
-        body: Container(
-          constraints: const BoxConstraints.expand(),
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("images/family-law.png"),
-                  fit: BoxFit.cover)),
-          /* child: const TextField(
-              decoration:
-                 // InputDecoration(fillColor: Colors.green, filled: true),
-            )*/
-        ));
+        actions: [
+          Container(
+            alignment: Alignment.centerRight,
+            child: Text(
+              user.Email,
+              style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+              padding: const EdgeInsets.all(10.0),
+              child: const Text(
+                ''' About Us: ''',
+                textAlign: TextAlign.center,
+                maxLines: 20,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              )),
+          Center(
+            child: Column(
+              children: [Image.asset('Images/paternity.jpg')],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            ''' ”Paternity Test” is an application dedicated to proving parentage and detecting child relevance. The most accurate aspect in determining the parents of a child is their DNA, to address one of the most significant phenomena that have spread around the world, that is homeless children. Orphaned children, and men who dispute the paternity of a child. We developed a method that analyzes the whole genome or alleles in a specific rs number. The proposed approach utilizes numerous algorithms to compare it to all of the individuals in the dataset. We applied methods to prove the parentage. Firstly, Mendelian's` law. Secondly, Short tandem repeat algorithm. Then, define the family of the orphan child. Additionally, the approach can determine whether or not the child is connected to his parents. Our experiment succeeded in proving or rejecting the relationship between the parent and child using the two approaches; either with RS or the whole genome.''',
+            textAlign: TextAlign.center,
+            maxLines: 20,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
