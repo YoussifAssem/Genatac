7961fc43babe460880a0fb118e5ded360d2ca304
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2022-06-27 00:47:11 +0200

    wholeExome
    Add wholeExome to the GUI

--- E-Genetics/Code/Models/whole.py
+++ E-Genetics/Code/Models/whole.py
@@ -1,49 +1,13 @@
-
-
-CounterTTTT = 0
-CounterAGAT = 0
-CounterAATG = 0
-CounterTGCC = 0
-CounterGATA = 0
-CounterTATC = 0
-CounterAAGG = 0
-CounterAGAA = 0
-
-
-def Test(x):
-    if x == "TTTT":
-        wholeGenome['TTTT'] += 1
-    elif x == "AGAT":
-        wholeGenome['AGAT'] += 1
-    elif x == "AATG":
-        wholeGenome['AATG'] += 1
-    elif x == "TGCC":
-        wholeGenome['TGCC'] += 1
-    elif x == "GATA":
-        wholeGenome['GATA'] += 1
-    elif x == "TATC":
-        wholeGenome['TATC'] += 1
-    elif x == "AAGG":
-        wholeGenome['AAGG'] += 1
-    elif x == "AGAA":
-        wholeGenome['AGAA'] += 1
-
-
-def readFile(fileName):
-    with open(fileName, 'r') as file:
-        data = file.read().rstrip()
-        n = 4    # every 2 characters
-    split_string = [data[i:i+n] for i in range(0, len(data), n)]
-    return split_string
-
-
-def reInitialize(wholeGenome):
-    for key in wholeGenome.keys():
-        wholeGenome[key] = 0
-
-
-if __name__ == '__main__':
-    wholeGenome = {
+class wholeGenome:
+      CounterTTTT = 0
+      CounterAGAT = 0
+      CounterAATG = 0
+      CounterTGCC = 0
+      CounterGATA = 0
+      CounterTATC = 0
+      CounterAAGG = 0
+      CounterAGAA = 0
+      __wholeGenome = {
         'TTTT': CounterTTTT,
         'AGAT': CounterAGAT,
         'AATG': CounterAATG,
@@ -53,73 +17,69 @@ if __name__ == '__main__':
         'AAGG': CounterAAGG,
         'AGAA': CounterAGAA
     }
+      def Test(self, x):
+            if x == "TTTT":
+                  self.__wholeGenome['TTTT'] += 1
+            elif x == "AGAT":
+                  self.__wholeGenome['AGAT'] += 1
+            elif x == "AATG":
+                  self.__wholeGenome['AATG'] += 1
+            elif x == "TGCC":
+                  self.__wholeGenome['TGCC'] += 1
+            elif x == "GATA":
+                  self.__wholeGenome['GATA'] += 1
+            elif x == "TATC":
+                  self.__wholeGenome['TATC'] += 1
+            elif x == "AAGG":
+                  self.__wholeGenome['AAGG'] += 1
+            elif x == "AGAA":
+                  self.__wholeGenome['AGAA'] += 1
 
 
-for x in readFile('dad2.txt'):
-    Test(x)
-    doc = wholeGenome.copy()
-
-print('Before Running Daughter', wholeGenome)
-
-
-reInitialize(wholeGenome)
-
-for x in readFile('dau2.txt'):
-    Test(x)
-
-print('After Running Daughter', wholeGenome)
-print(doc['TTTT'])
-print(wholeGenome['TTTT'])
+      def readFile(fileName):
+            with open(fileName, 'r') as file:
+                  data = file.read().rstrip()
+                  n = 4    # every 2 characters
+            split_string = [data[i:i+n] for i in range(0, len(data), n)]
+            return split_string
 
 
-decision = 0
+      def reInitialize(self):
+            for key in self.__wholeGenome.keys():
+                  self.__wholeGenome[key] = 0
 
-if (wholeGenome['AGAA']-100000 <= doc['AGAA'] <= wholeGenome['AGAA']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
+      def getResults(self, doc):
+            decision = 0
+            if ( self.__wholeGenome['AGAA']-100000 <= doc['AGAA'] <=  self.__wholeGenome['AGAA']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['TTTT']-100000 <= doc['TTTT'] <=  self.__wholeGenome['TTTT']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['AGAT']-100000 <= doc['AGAT'] <=  self.__wholeGenome['AGAT']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['AATG']-100000 <= doc['AATG'] <=  self.__wholeGenome['AATG']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['TGCC']-100000 <= doc['TGCC'] <=  self.__wholeGenome['TGCC']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['GATA']-100000 <= doc['GATA'] <=  self.__wholeGenome['GATA']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['TATC']-100000 <= doc['TATC'] <=  self.__wholeGenome['TATC']+100000):
+                  decision = decision+1
+                  print("Match")
+            if ( self.__wholeGenome['AAGG']-100000 <= doc['AAGG'] <=  self.__wholeGenome['AAGG']+100000):
+                  decision = decision+1
+                  print("Match")
+            else:
+                  print("No Match")
 
-if (wholeGenome['TTTT']-100000 <= doc['TTTT'] <= wholeGenome['TTTT']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-if (wholeGenome['AGAT']-100000 <= doc['AGAT'] <= wholeGenome['AGAT']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-if (wholeGenome['AATG']-100000 <= doc['AATG'] <= wholeGenome['AATG']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-if (wholeGenome['TGCC']-100000 <= doc['TGCC'] <= wholeGenome['TGCC']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-if (wholeGenome['GATA']-100000 <= doc['GATA'] <= wholeGenome['GATA']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-if (wholeGenome['TATC']-100000 <= doc['TATC'] <= wholeGenome['TATC']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-if (wholeGenome['AAGG']-100000 <= doc['AAGG'] <= wholeGenome['AAGG']+100000):
-    decision = decision+1
-    print("Match")
-else:
-    print("No Match")
-
-
-if(decision >= 4):
-    print("According to the whole genome you entered ,the 2 people you entered is RELATED")
-elif(decision <= 2 and decision != 0):
-    print("According to the whole genome you entered, the 2 people you entered has 2 matching repeats but NOT RELATED")
-elif(decision == 0):
-    print("According to the whole genome you entered, the 2 people you entered is NOT RELATED")
+            if(decision >= 4):
+                  print("According to the whole genome you entered ,the 2 people you entered is RELATED")
+            elif(decision <= 2 and decision != 0):
+                  print("According to the whole genome you entered, the 2 people you entered has 2 matching repeats but NOT RELATED")
+            elif(decision == 0):
+                  print("According to the whole genome you entered, the 2 people you entered is NOT RELATED")
